1537197b73c4136a4fd00dcacbc9437971f0876f
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-04 03:01:11 +0900

    172解答

---
+++ ABC172/ProgramA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + a * a + a * a * a);
+        }
+    }
+}
