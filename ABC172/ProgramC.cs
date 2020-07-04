f73e368c277560ae2775b0212bbdf7e75f0e7de4
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-05 02:24:00 +0900

    172 CD解説

---
+++ ABC172/ProgramC.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = int.Parse(inputs[0]);
+            var m = int.Parse(inputs[1]);
+            var k = int.Parse(inputs[2]);
+
+            var aList = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            var bList = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+
+            var minute = 0L;
+            var i = 0;
+            var j = 0;
+            var count = 0;
+
+            // bmax
+            minute = bList.Sum();
+            j = m;
+
+            // aを増やすためのループ
+            for (i = 0; i < n + 1; i++)
+            {
+                // b減らす
+                while (minute > k && j > 0)
+                {
+                    j--;
+                    minute -= bList[j];
+                }
+
+                // aだけで時間超過している場合終了
+                if (minute > k) break;
+
+                // 更新
+                count = Math.Max(count, i + j);
+
+                // 最後の更新が終わったら離脱
+                if (i == n) break;
+
+                // a増やす
+                minute += aList[i];
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ABC172/ProgramD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ABC
+{
+    class ProgramD
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = 0L;
+
+            for (var i = 1; i <= n; i++)
+            {
+                // var y = yakusu2(i);
+                // // Console.WriteLine(i + ", " + y);
+                // result += y * i;
+
+                // 純粋に約数を求めるのと同じ計算で約数の個数を求めると処理速度的に追いつかない
+                // nまでの値をiで割りきれた場合、iを加算する方式にする
+                // (この方法は約数そのものはわからないが個数は取得できる)
+                result += i * sumI(n / i);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        // 約数の個数を求める O(N)
+        // static int yakusu(int n)
+        // {
+        //     var count = 0;
+        //     for (var i = 1; i <= n; i++)
+        //     {
+        //         if (n % i == 0)
+        //         {
+        //             count++;
+        //         }
+        //     }
+        //     return count;
+        // }
+
+        // 約数の個数を求める O(sqrt(N))
+        // static int yakusu2(int n)
+        // {
+        //     var count = 0;
+        //     for (var i = 1; i * i <= n; i++)
+        //     {
+        //         if (n % i == 0)
+        //         {
+        //             count++;
+        //             if (i * i != n)
+        //                 count++;
+        //         }
+        //     }
+        //     return count;
+        // }
+
+        // 総和
+        static long sumI(long i)
+        {
+            return (i * (i + 1)) / 2;
+        }
+    }
+}
