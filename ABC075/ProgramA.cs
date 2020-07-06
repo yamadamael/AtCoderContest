0e02037a74b711e07c35153a8e91d00bf56e1776
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-06 13:52:42 +0900

    精進 ABC075 AB

---
+++ ABC075/ProgramA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            var n = list.Count;
+
+            var dist = list.Distinct();
+            foreach (var d in dist)
+            {
+                if (list.Count(x => x == d) == 1)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
