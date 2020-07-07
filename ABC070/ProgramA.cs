fdeac182b3a41c21c0b684cf2296aaf731a32e1d
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-08 02:08:17 +0900

    精進 ABC070 AB解答、C学習

---
+++ ABC070/ProgramA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var a = long.Parse(Console.ReadLine());
+            var list = a.ToString().ToCharArray().Reverse();
+            var rs = "";
+            foreach (var c in list)
+            {
+                rs += c.ToString();
+            }
+            var ra = int.Parse(rs);
+
+            Console.WriteLine(a == ra ? "Yes" : "No");
+        }
+    }
+}
