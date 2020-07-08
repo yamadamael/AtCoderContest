52e31e999d01779f5c4051cc9d5af7f9aea23859
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-08 17:40:17 +0900

    精進 ABC048 ABC解答、D解説

---
+++ ABC048/ProgramA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var result = inputs[0][0].ToString() + inputs[1][0] + inputs[2][0];
+            Console.WriteLine(result);
+        }
+    }
+}
