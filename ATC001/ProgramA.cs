a60f54c3b70d663d7114e43b7dd562afdb17e834
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-09 18:18:56 +0900

    精進 ATC001 AB学習

---
+++ ATC001/ProgramA.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static bool[,] _seen;
+        static int[] _dx = new int[] { 0, 1, 0, -1 };
+        static int[] _dy = new int[] { 1, 0, -1, 0 };
+        static long _h, _w;
+        static string[] _field;
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            _h = long.Parse(inputs[0]);
+            _w = long.Parse(inputs[1]);
+
+            _field = new string[_h];
+            int sh = 0, sw = 0, gh = 0, gw = 0;
+            for (var i = 0; i < _h; i++)
+            {
+                _field[i] = Console.ReadLine();
+                for (var j = 0; j < _w; j++)
+                {
+                    if (_field[i][j] == 's') { sh = i; sw = j; }
+                    if (_field[i][j] == 'g') { gh = i; gw = j; }
+                }
+            }
+
+            _seen = new bool[_h, _w];
+            dfs(sh, sw);
+
+            // Console.WriteLine(string.Format("({0},{1}), ({2},{3})", sh, sw, gh, gw));
+            Console.WriteLine(_seen[gh, gw] ? "Yes" : "No");
+        }
+
+        static void dfs(int h, int w)
+        {
+            _seen[h, w] = true;
+            for (var i = 0; i < 4; i++)
+            {
+                var nh = h + _dy[i];
+                var nw = w + _dx[i];
+
+                // 範囲外
+                if (nh < 0 || _h <= nh) continue;
+                if (nw < 0 || _w <= nw) continue;
+
+                // 通過済みか
+                if (_seen[nh, nw]) continue;
+
+                // .ですか
+                if (_field[nh][nw] == '#') continue;
+
+                dfs(nh, nw);
+            }
+        }
+    }
+}
---
+++ ATC001/ProgramB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static int[] par, rank;
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = int.Parse(inputs[0]);
+            var q = int.Parse(inputs[1]);
+
+            init(n);
+            for (var i = 0; i < q; i++)
+            {
+                var inputsQ = Console.ReadLine().Split(" ");
+                var p = int.Parse(inputsQ[0]);
+                var a = int.Parse(inputsQ[1]);
+                var b = int.Parse(inputsQ[2]);
+
+                if (p == 0)
+                {
+                    // 連結
+                    unite(a, b);
+                }
+                else
+                {
+                    // 判定
+                    Console.WriteLine(same(a, b) ? "Yes" : "No");
+                }
+            }
+        }
+
+        // 初期化
+        // par[i]=iの場合、根である
+        static void init(int n)
+        {
+            par = new int[n];
+            rank = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                par[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        // 木の根を求める
+        static int root(int x)
+        {
+            if (par[x] == x)
+            {
+                return x;
+            }
+            else
+            {
+                par[x] = root(par[x]);
+                return par[x];
+            }
+        }
+
+        // 2頂点が同じ集合か
+        static bool same(int x, int y)
+        {
+            return root(x) == root(y);
+        }
+
+        // 集合の併合(ランク考慮あり)
+        static void unite(int x, int y)
+        {
+            var nx = root(x);
+            var ny = root(y);
+            if (nx == ny) return;
+            if (rank[nx] < rank[ny])
+            {
+                par[nx] = ny;
+            }
+            else
+            {
+                par[ny] = nx;
+                if (rank[nx] == rank[ny])
+                    rank[nx]++;
+            }
+        }
+    }
+}
