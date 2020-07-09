25a0a1192e786f7cc309b9d1bc4ecebf6c2febec
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-09 18:20:22 +0900

    精進 ABC060 ABC解答

---
+++ ABC060/ProgramA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var a = inputs[0];
+            var b = inputs[1];
+            var c = inputs[2];
+
+            if (a[a.Length - 1] != b[0] || b[b.Length - 1] != c[0])
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
---
+++ ABC060/ProgramC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var t = long.Parse(inputs[1]);
+
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+
+            var result = 0L;
+
+            for (var i = 1; i < n; i++)
+            {
+                var d = list[i] - list[i - 1];
+                result += d > t ? t : d;
+            }
+            result += t;
+
+            Console.WriteLine(result);
+        }
+    }
+}
