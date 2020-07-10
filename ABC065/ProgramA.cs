dfb2e1bb3db24ccf7d145260b09a79b95fb0f708
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-10 18:44:17 +0900

    精進 ABC065 AB解答、C解説

---
+++ ABC065/ProgramA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var x = long.Parse(inputs[0]);
+            var a = long.Parse(inputs[1]);
+            var b = long.Parse(inputs[2]);
+
+            if (a >= b)
+            {
+                Console.WriteLine("delicious");
+            }
+            else if (a + x >= b)
+            {
+                Console.WriteLine("safe");
+            }
+            else
+            {
+                Console.WriteLine("dangerous");
+            }
+        }
+    }
+}
---
+++ ABC065/ProgramC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var m = long.Parse(inputs[1]);
+
+            if (Math.Abs(n - m) > 1)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            long nn = 1, nm = 1;
+            for (var i = n; i > 1; i--)
+            {
+                nn = (nn * i) % (long)(1e9 + 7);
+            }
+
+            for (var i = m; i > 1; i--)
+            {
+                nm = (nm * i) % (long)(1e9 + 7);
+            }
+
+            var key = nn == nm ? 2 : 1;
+
+            Console.WriteLine((nn * nm) % ((long)1e9 + 7) * key % ((long)1e9 + 7));
+        }
+    }
+}
