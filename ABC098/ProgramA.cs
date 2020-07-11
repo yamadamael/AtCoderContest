61ccb0945ed6808e301b306097d4b0eb417935ac
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-11 18:33:14 +0900

    精進 ABC098 AB解答、C学習

---
+++ ABC098/ProgramA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = int.Parse(inputs[0]);
+            var m = int.Parse(inputs[1]);
+
+            var list = new int[] { n + m, n - m, n * m };
+            Console.WriteLine(list.Max());
+        }
+    }
+}
