1e0b5dc0c4d789ff294936c40b4f71a32dbc2f28
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-14 01:24:59 +0900

    精進 ABC064 ABC解答

---
+++ ABC054/ProgramB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static string[] _mGraph;
+        static string[] _nGraph;
+
+        static int[] _dx = { 1, 0 };
+        static int[] _dy = { 0, 1 };
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = int.Parse(inputs[0]);
+            var m = int.Parse(inputs[1]);
+
+            _nGraph = new string[n];
+            _mGraph = new string[m];
+            for (var i = 0; i < n; i++)
+                _nGraph[i] = Console.ReadLine();
+            for (var i = 0; i < m; i++)
+                _mGraph[i] = Console.ReadLine();
+
+            for (var ny = 0; ny <= n - m; ny++)
+            {
+                for (var nx = 0; nx <= n - m; nx++)
+                {
+                    if (_nGraph[nx][ny] != _mGraph[0][0]) continue;
+                    if (search(nx, ny, m))
+                    {
+                        Console.WriteLine("Yes");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("No");
+        }
+
+        static bool search(int nx, int ny, int m)
+        {
+            for (var my = 0; my < m; my++)
+            {
+                for (var mx = 0; mx < m; mx++)
+                {
+                    // Console.WriteLine(string.Format("({0},{1})({2},{3})", nx, ny, mx, my));
+                    if (_nGraph[ny + my][nx + mx] != _mGraph[my][mx])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ABC054/ProgramC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        struct Info
+        {
+            public int to;
+            public string path;
+            // public string ind;
+        }
+        static Queue<Info> _q = new Queue<Info>();
+        static List<int>[] _graph;
+        static int _count;
+        static int _n;
+
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            _n = int.Parse(inputs[0]);
+            var m = int.Parse(inputs[1]);
+
+            _graph = new List<int>[_n];
+            for (var i = 0; i < _n; i++)
+            {
+                _graph[i] = new List<int>();
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                var inputs1 = Console.ReadLine().Split(" ");
+                var a = int.Parse(inputs1[0]) - 1;
+                var b = int.Parse(inputs1[1]) - 1;
+                _graph[a].Add(b);
+                _graph[b].Add(a);
+            }
+
+            _q.Enqueue(new Info() { to = 0, path = "0" });
+            while (_q.Count > 0)
+            {
+                var info = _q.Dequeue();
+                var v = info.to;
+                var path = info.path;
+                // var ind = info.ind;
+                // Console.WriteLine(ind + "v: " + v + ", path: " + path);
+
+                foreach (var t in _graph[v])
+                {
+                    if (path.Contains(t.ToString())) continue;
+                    _q.Enqueue(new Info()
+                    {
+                        to = t,
+                        path = path + t.ToString(),
+                        // ind = ind + "  "
+                    });
+                    // Console.WriteLine(ind + "  t: " + t);
+                }
+
+                if (path.Count() == _n)
+                    _count++;
+            }
+
+            Console.WriteLine(_count);
+        }
+    }
+}
