f48f5f25504678b6afd679a4094fe5637147e693
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-13 15:39:30 +0900

    精進 ABC002 ABCD解答

---
+++ ABC002/ProgramB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            s = s.Replace("a", "");
+            s = s.Replace("i", "");
+            s = s.Replace("u", "");
+            s = s.Replace("e", "");
+            s = s.Replace("o", "");
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ ABC002/ProgramC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var ax = long.Parse(inputs[0]);
+            var ay = long.Parse(inputs[1]);
+            var bx = long.Parse(inputs[2]) - ax;
+            var by = long.Parse(inputs[3]) - ay;
+            var cx = long.Parse(inputs[4]) - ax;
+            var cy = long.Parse(inputs[5]) - ay;
+            ax = ay = 0;
+
+            var result = (bx * cy - cx * by) / 2f;
+
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
