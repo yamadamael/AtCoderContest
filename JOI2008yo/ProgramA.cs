f33915cd96399754d91485635907dfc7e06200f1
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-14 14:01:05 +0900

    精進 JOI2008 AB解答

---
+++ JOI2008yo/ProgramA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var result = 0;
+
+            a = 1000 - a;
+
+            result += a / 500;
+            a = a % 500;
+
+            result += a / 100;
+            a = a % 100;
+
+            result += a / 50;
+            a = a % 50;
+
+            result += a / 10;
+            a = a % 10;
+
+            result += a / 5;
+            a = a % 5;
+
+            result += a;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ JOI2008yo/ProgramB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var joi = 0;
+            var ioi = 0;
+
+            for (var i = 2; i < s.Length; i++)
+            {
+                var t = s.Substring(i - 2, 3);
+                if (t == "JOI")
+                    joi++;
+                else if (t == "IOI")
+                    ioi++;
+            }
+
+            Console.WriteLine(joi);
+            Console.WriteLine(ioi);
+        }
+    }
+}
