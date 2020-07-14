211674114433ea08f3e27427a7fbad5c3f36ecb0
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-14 17:36:06 +0900

    精進 ABC011 ABC解答

---
+++ ABC011/ProgramB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = "";
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (i == 0)
+                    result += s[i].ToString().ToUpper();
+                else
+                    result += s[i].ToString().ToLower();
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
