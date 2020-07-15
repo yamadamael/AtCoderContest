71297737244fc98a9a189f99184fcad5325ae029
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-15 12:18:29 +0900

    精進 ABC103 AB解答

---
+++ ABC103/ProgramB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var t = Console.ReadLine();
+            var result = false;
+
+            s += s;
+
+            for (var i = 0; i < t.Length; i++)
+            {
+                var r = s.Substring(i, t.Length);
+                if (r == t)
+                {
+                    result = true;
+                }
+            }
+
+            Console.WriteLine(result ? "Yes" : "No");
+        }
+    }
+}
