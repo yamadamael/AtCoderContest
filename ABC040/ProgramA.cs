4cbdc42bef812da89654f1a653ed9a7528b16394
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-16 02:00:14 +0900

    精進 ABC040 ABC解答

---
+++ ABC040/ProgramA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var x = long.Parse(inputs[1]);
+            var result = Math.Min(x - 1, n - x);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC040/ProgramB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = int.Parse(Console.ReadLine());
+
+            var result = int.MaxValue;
+
+            for (var i = 1; i <= Math.Sqrt(s); i++)
+            {
+                var j = s / i;
+                result = Math.Min((s - (i * j)) + Math.Abs(i - j), result);
+            }
+
+            Console.WriteLine(s == 1 ? 0 : result);
+        }
+    }
+}
