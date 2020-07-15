792b3f4313382352bd68e99e8a274759392f2c02
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-15 12:52:50 +0900

    精進 ABC103 CD解説

--- ABC103/ProgramC.cs
+++ ABC103/ProgramC.cs
@@ -8,21 +8,9 @@ namespace ABC
     {
         static void Main(string[] args)
         {
-            var s = Console.ReadLine();
-
-            var a = long.Parse(Console.ReadLine());
-
-            var inputs = Console.ReadLine().Split(" ");
-            var n = long.Parse(inputs[0]);
-            var m = long.Parse(inputs[1]);
-            var k = long.Parse(inputs[2]);
-
+            var n = long.Parse(Console.ReadLine());
             var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
-
-            var result = 0;
-
-
-
+            var result = list.Sum() - n;
             Console.WriteLine(result);
         }
     }
