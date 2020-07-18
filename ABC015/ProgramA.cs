9ed114e2a57885bda8f2a0d93473a541784b1552
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-18 19:09:27 +0900

    精進 ABC015 AB解答、C解説

---
+++ ABC015/ProgramA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var t = Console.ReadLine();
+            Console.WriteLine(s.Length > t.Length ? s : t);
+        }
+    }
+}
---
+++ ABC015/ProgramB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            var list = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
+            list = list.Where(x => x > 0).ToList();
+            var result = Math.Ceiling(list.Average());
+            Console.WriteLine(result);
+        }
+    }
+}
