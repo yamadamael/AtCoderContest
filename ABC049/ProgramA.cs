5fa6e595d5649304e1ce3d2b7d1c38700f66ea9c
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-20 00:52:16 +0900

    精進 ABC049 ABC解答、D解説

---
+++ ABC049/ProgramA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = s == "a" || s == "i" || s == "u" || s == "e" || s == "o";
+            Console.WriteLine(result ? "vowel" : "consonant");
+        }
+    }
+}
---
+++ ABC049/ProgramC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var list = new string[] { "dream", "dreamer", "erase", "eraser" };
+
+            while (s != "")
+            {
+                var t = s;
+                for (var i = 0; i < 4; i++)
+                {
+                    if (s.Length - list[i].Length < 0)
+                        continue;
+                    var sub = s.Substring(s.Length - list[i].Length, list[i].Length);
+                    if (sub == list[i])
+                    {
+                        s = s.Substring(0, s.Length - list[i].Length);
+                        // Console.WriteLine(s);
+                    }
+                }
+                if (s == t)
+                    break;
+            }
+
+            Console.WriteLine(s == "" ? "YES" : "NO");
+        }
+    }
+}
