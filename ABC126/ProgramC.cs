a2f25641804310ed1694ea7e892d6cc8b0d325b1
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-21 02:11:39 +0900

    精進 ABC126 ABC解答

---
+++ ABC126/ProgramC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var k = long.Parse(inputs[1]);
+
+            var result = 0d;
+            for (var i = 1L; i <= n; i++)
+            {
+                var m = i;
+                // var count = 0;
+                var tmp = 1d / n;
+                while (m < k)
+                {
+                    m = m << 1;
+                    tmp /= 2;
+                    // count++;
+                }
+
+                result += tmp;
+                // Console.WriteLine(string.Format("{0},{1}", i, count));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
