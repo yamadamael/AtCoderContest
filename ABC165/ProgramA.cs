d2c2c9e58ae8d58f60a6f293d111d9e317b18e7b
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-24 22:49:47 +0900

    精進 ABC165 ABD解答、C解説

---
+++ ABC165/ProgramA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var k = long.Parse(Console.ReadLine());
+            var inputs = Console.ReadLine().Split(" ");
+            var a = long.Parse(inputs[0]);
+            var b = long.Parse(inputs[1]);
+            var result = false;
+
+            for (var i = a; i <= b; i++)
+            {
+                if (i % k == 0)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            Console.WriteLine(result ? "OK" : "NG");
+        }
+    }
+}
---
+++ ABC165/ProgramB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var x = long.Parse(Console.ReadLine());
+            var result = 0L;
+            var a = 100L;
+            while (a < x)
+            {
+                a = a + (a / 100L);
+                result++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
