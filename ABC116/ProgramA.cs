946e79762ff21e4771aca74974b052924fbcd889
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-25 20:53:14 +0900

    精進 ABC116 ABC解答

---
+++ ABC116/ProgramA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var ab = long.Parse(inputs[0]);
+            var bc = long.Parse(inputs[1]);
+            var ca = long.Parse(inputs[2]);
+            var result = (bc * ab) / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC116/ProgramC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            var h = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
+            var l = new int[n];
+
+            var result = 0;
+            for (var i = 0; i < n; i++)
+            {
+                while (l[i] < h[i])
+                {
+                    result++;
+                    var count = i;
+                    // Console.WriteLine(string.Format("{0},{1}", i, count));
+                    while (count < n && l[count] < h[count])
+                    {
+                        l[count]++;
+                        // Console.WriteLine(string.Format("  {0},{1}", count, l[count]));
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
