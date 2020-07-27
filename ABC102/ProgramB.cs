d00e4b076199d5a5096405bbbbb42bf8ccf5bb28
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-28 00:51:28 +0900

    精進 ABC102 AB解答、C解説

---
+++ ABC102/ProgramB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var a = long.Parse(Console.ReadLine());
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            var result = list.Max() - list.Min();
+            Console.WriteLine(result);
+        }
+    }
+}
