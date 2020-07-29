ea8cb6efe882f895b53ec651423dc4072d772ee7
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-29 11:29:36 +0900

    精進 m-solution2020 E解説途中

--- m-solution2020/ProgramE.cs
+++ m-solution2020/ProgramE.cs
@@ -6,24 +6,46 @@ namespace ABC
 {
     class ProgramE
     {
+
+        static (int, int, int)[] _list;
         static void Main(string[] args)
         {
-            var s = Console.ReadLine();
-
-            var a = long.Parse(Console.ReadLine());
-
-            var inputs = Console.ReadLine().Split(" ");
-            var n = long.Parse(inputs[0]);
-            var m = long.Parse(inputs[1]);
-            var k = long.Parse(inputs[2]);
-
-            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            var n = int.Parse(Console.ReadLine());
+            _list = new (int, int, int)[n];
+            for (var i = 0; i < n; i++)
+            {
+                var inputs = Console.ReadLine().Split(" ");
+                var x = int.Parse(inputs[0]);
+                var y = int.Parse(inputs[1]);
+                var p = int.Parse(inputs[2]);
+                _list[i] = (x, y, p);
+            }
 
             var result = 0;
 
-
-
             Console.WriteLine(result);
+        }
+
+        // 0 none
+        // 1 hori
+        // 2 vart
+        static int _m = 3;
+        static void dfs(Stack<int> l, int n)
+        {
+            if (n <= l.Count)
+            {
+                for (var i = 0; i < l.Count; i++)
+                {
+
+                }
+            }
+
+            for (var i = 0; i < _m; i++)
+            {
+                l.Push(i);
+                dfs(l, n);
+                l.Pop();
+            }
         }
     }
 }
---
+++ m-solution2020/ProgramE2.cs
@@ -0,0 +1,83 @@
+﻿// 解説のサンプル写経
+// https://atcoder.jp/contests/m-solutions2020/submissions/15122924
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramE2
+    {
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var list = new (int, int, int)[n];
+            for (var i = 0; i < n; i++)
+            {
+                var inputs = Console.ReadLine().Split(" ");
+                var x = int.Parse(inputs[0]);
+                var y = int.Parse(inputs[1]);
+                var p = int.Parse(inputs[2]);
+                list[i] = (x, y, p);
+            }
+
+            var result = solve(n, list);
+
+            foreach (var x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static long[] solve(int n, (int, int, int)[] list)
+        {
+            var xsel = new int[1 << n, n];
+            var ysel = new int[1 << n, n];
+
+            for (var i = 0; i < (1 << n); i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    xsel[i, j] = Math.Abs(list[j].Item1);
+                    ysel[i, j] = Math.Abs(list[j].Item2);
+                    for (var k = 0; k < n; k++)
+                    {
+                        if (((i >> k) & 1) == 1)
+                        {
+                            xsel[i, j] = Math.Min(xsel[i, j], Math.Abs(list[j].Item1 - list[k].Item1));
+                            ysel[i, j] = Math.Min(ysel[i, j], Math.Abs(list[j].Item2 - list[k].Item2));
+                        }
+                    }
+                }
+            }
+
+            var result = Enumerable.Repeat(long.MaxValue, n + 1).ToArray();
+            for (var i = 0; i < (1 << n); i++)
+            {
+                var cnt = 0;
+                for (var j = 0; j < n; j++)
+                {
+                    if (((i >> j) & 1) == 1) cnt++;
+                }
+
+                for (var j = i; j >= 0; j--)
+                {
+                    j &= i;
+                    var cost = 0L;
+                    for (var k = 0; k < n; k++)
+                    {
+                        if (((i >> k) & 1) == 0)
+                        {
+                            cost += 1L * Math.Min(xsel[j, k], ysel[j, k]) * list[k].Item3;
+                        }
+                    }
+                    result[cnt] = Math.Min(result[cnt], cost);
+                }
+            }
+
+            return result;
+        }
+    }
+}
