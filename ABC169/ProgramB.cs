a9e904ec863482505d63a3f3e656798c99189ae2
yamadakuniaki <kuniakiymd@gmail.com>
2020-07-31 11:29:57 +0900

    精進 ABC169 AB解答、C解説

---
+++ ABC169/ProgramB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            if (list.Contains(0))
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var result = 1L;
+            for (var i = 0; i < n; i++)
+            {
+                var r = 1e18 / list[i];
+                if (r < result)
+                {
+                    result = -1;
+                    break;
+                }
+                result *= list[i];
+            }
+
+            if (result > (long)1e18)
+                Console.WriteLine(-1);
+            else
+                Console.WriteLine(result);
+        }
+    }
+}
