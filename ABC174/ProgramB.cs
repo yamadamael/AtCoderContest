66143146916c772950a39bf7d378a3a5b86ab4bf
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-04 23:12:02 +0900

    ABC174 ABD解答、C解説

---
+++ ABC174/ProgramB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var d = long.Parse(inputs[1]);
+            var result = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                var inputs1 = Console.ReadLine().Split(" ");
+                var x = long.Parse(inputs1[0]);
+                var y = long.Parse(inputs1[1]);
+
+                var dist = Math.Sqrt(x * x + y * y);
+                if (dist <= d) result++;
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
