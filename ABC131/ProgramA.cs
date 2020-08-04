9926f9e640447ca2de32b1037f2dcfe8b6653291
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-04 23:15:06 +0900

    精進 ABC131 AB解答

---
+++ ABC131/ProgramA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = s[0] == s[1] || s[1] == s[2] || s[2] == s[3];
+            Console.WriteLine(result ? "Bad" : "Good");
+        }
+    }
+}
---
+++ ABC131/ProgramB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = int.Parse(inputs[0]);
+            var l = int.Parse(inputs[1]);
+            var result = 0;
+
+            var min = l;
+            for (var i = 0; i < n; i++)
+            {
+                result += i + l;
+                min = Math.Abs(min) < Math.Abs(i + l) ? min : i + l;
+            }
+
+            Console.WriteLine(result - min);
+        }
+    }
+}
