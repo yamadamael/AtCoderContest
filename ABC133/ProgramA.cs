e084e330a48e4c2b27aa4d91b169c4ba1da6e26e
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-06 23:03:54 +0900

    精進 ABC133 AB解答、C解説

---
+++ ABC133/ProgramA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var a = long.Parse(inputs[1]);
+            var b = long.Parse(inputs[2]);
+            var result = Math.Min(a * n, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC133/ProgramC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var l = long.Parse(inputs[0]);
+            var r = long.Parse(inputs[1]);
+            // var list = new List<long>();
+
+            // for (var i = l; i <= r; i++)
+            // {
+            //     var m = i % 2019;
+            //     if (list.Contains(m))
+            //     {
+            //         break;
+            //     }
+            //     Console.WriteLine(string.Join(",", i, m));
+            //     list.Add(m);
+            // }
+
+            // list.Sort();
+            // Console.WriteLine(string.Join(",", list[0], list[1]));
+            // var result = (list[0] * list[1]) % 2019;
+            // Console.WriteLine(result);
+
+            if (r - l > 3000)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var min = 2020L;
+            for (var i = l; i < r; i++)
+            {
+                for (var j = i + 1; j <= r; j++)
+                {
+                    var m = (i * j) % 2019;
+                    min = Math.Min(min, m);
+                    if (min == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
