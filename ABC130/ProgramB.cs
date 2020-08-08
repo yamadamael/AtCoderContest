f9b30525014b998d06604d4f786c2622fbce45aa
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-08 17:31:11 +0900

    精進 ABC130 AB解答

---
+++ ABC130/ProgramB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var x = long.Parse(inputs[1]);
+
+            var list = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
+
+            var result = 1;
+            var d = new int[n + 1];
+            for (var i = 1; i < n + 1; i++)
+            {
+                d[i] = d[i - 1] + list[i - 1];
+                if (d[i] > x)
+                {
+                    break;
+                }
+                result++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
