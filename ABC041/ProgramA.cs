052a95fda187732a00465e4c3819210a4d647647
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-14 19:41:18 +0900

    精進 ABC041 AB解答

---
+++ ABC041/ProgramA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var i = int.Parse(Console.ReadLine());
+            var result = s[i - 1];
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC041/ProgramB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var a = long.Parse(inputs[0]);
+            var b = long.Parse(inputs[1]);
+            var c = long.Parse(inputs[2]);
+            var result = a * b % (long)(1e9 + 7);
+            result = result * c % (long)(1e9 + 7);
+            Console.WriteLine(result);
+        }
+    }
+}
