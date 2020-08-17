63954c827b8501918d743aaf0d4e44ac52664440
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-17 23:52:09 +0900

    精進 ABC089 AB解答

---
+++ ABC089/ProgramB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var a = long.Parse(Console.ReadLine());
+            var list = Console.ReadLine().Split(" ").Select(x => x).ToList();
+            list = list.Distinct().ToList();
+            var result = list.Count == 3 ? "Three" : "Four";
+            Console.WriteLine(result);
+        }
+    }
+}
