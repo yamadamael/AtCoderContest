ff53f951e5973e1c269c574465e53f792f2b95ef
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-18 00:12:41 +0900

    精進 ABC068 A解答

---
+++ ABC068/ProgramA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var a = long.Parse(Console.ReadLine());
+            var result = "ABC" + a;
+           Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC068/ProgramB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+
+            var a = long.Parse(Console.ReadLine());
+
+            var inputs = Console.ReadLine().Split(" ");
+            var n = long.Parse(inputs[0]);
+            var m = long.Parse(inputs[1]);
+            var k = long.Parse(inputs[2]);
+
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+
+            var result = 0;
+
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
