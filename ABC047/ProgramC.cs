3ba33cf33f7015c8108ca07bb3dcfaaa3b13fa90
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-27 19:11:22 +0900

    精進 ABC047 ABC解答

---
+++ ABC047/ProgramC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().ToCharArray().Select(x => x.ToString()).ToList();
+            var result = 0;
+            var current = s[0];
+
+            for (var i = 0; i < s.Count; i++)
+            {
+                if(s[i] != current)
+                {
+                    result++;
+                    current = s[i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
