6ba7bb87b9d3c580955f91887c133b1a3a5f1bee
yamadakuniaki <kuniakiymd@gmail.com>
2020-08-28 20:32:21 +0900

    精進 ABC093 A解答

---
+++ ABC093/ProgramA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = true;
+            result = result && s.Contains("a");
+            result = result && s.Contains("b");
+            result = result && s.Contains("c");
+            Console.WriteLine(result ? "Yes" : "No");
+        }
+    }
+}
