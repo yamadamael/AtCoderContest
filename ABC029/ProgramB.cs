8d545a4070588c4b63f96392ebbe596cd668746e
yamadakuniaki <kuniakiymd@gmail.com>
2020-09-03 00:04:16 +0900

    精進 ABC029 AB解答

---
+++ ABC029/ProgramB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            for (var i = 0; i < 12; i++)
+            {
+                var s = Console.ReadLine();
+                result = s.Contains('r') ? result + 1 : result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
