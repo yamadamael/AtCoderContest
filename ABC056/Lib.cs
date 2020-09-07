09d69e913b8ad59fb6a35eb2e0670f281f15e5b0
yamadakuniaki <kuniakiymd@gmail.com>
2020-09-07 23:33:34 +0900

    精進 ABC056 A解答

---
+++ ABC056/Lib.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    public class Lib
+    {
+        // 約数リストを取得する
+        // O(sqrt(N))
+        static List<long> Divisor(long n)
+        {
+            var result = new List<long>();
+            for (var i = 1; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    result.Add(i);
+                    if (i * i != n)
+                        result.Add(n / i);
+                }
+            }
+            return result;
+        }
+
+        // 総和
+        // Ei
+        static long SumI(long i)
+        {
+            return (i * (i + 1)) / 2;
+        }
+    }
+}
