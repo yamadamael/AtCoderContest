564f4c5e0b8fa68c514167799ce6c49ce9bbfa8b
yamadakuniaki <kuniakiymd@gmail.com>
2020-09-13 02:45:03 +0900

    精進 ABC094 A解答

---
+++ ABC094/ProgramA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var a = long.Parse(inputs[0]);
+            var b = long.Parse(inputs[1]);
+            var x = long.Parse(inputs[2]);
+            var result = a <= x && x <= a + b ? "YES" : "NO";
+            Console.WriteLine(result);
+        }
+    }
+}
