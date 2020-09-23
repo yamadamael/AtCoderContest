285739f2fb862a4bda5cc0c4b7be9eb415e17180
yamadakuniaki <kuniakiymd@gmail.com>
2020-09-23 23:07:52 +0900

    精進 ABC154 ABC解答、D沼

---
+++ ABC154/ProgramB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = "";
+            for (var i = 0; i < s.Length; i++)
+            {
+                result += "x";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC154/ProgramC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var a = long.Parse(Console.ReadLine());
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            var list2 = list.Distinct().ToList();
+            Console.WriteLine(list.Count == list2.Count ? "YES" : "NO");
+        }
+    }
+}
