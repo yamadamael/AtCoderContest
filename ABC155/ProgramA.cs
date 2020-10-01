c38d02d3e9742fc2d866121655daf7892e436187
yamadakuniaki <kuniakiymd@gmail.com>
2020-10-01 23:09:09 +0900

    精進 ABC155 ABC解答

---
+++ ABC155/ProgramA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+            list = list.Distinct().ToList();
+            var result = list.Count == 2 ? "Yes" : "No"; ;
+            Console.WriteLine(result);
+        }
+    }
+}
