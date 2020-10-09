6af883ad62fa58f8ba0f4ad8b97ae61fa2a28334
yamadakuniaki <kuniakiymd@gmail.com>
2020-10-09 22:56:36 +0900

    精進 ABC101 ABC解答

---
+++ ABC101/ProgramA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramA
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var result = 0L;
+
+            result += s.Count(x => x == '+');
+            result -= s.Count(x => x == '-');
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC101/ProgramB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var n = int.Parse(s);
+            var sn = 0L;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                sn += int.Parse(s[i].ToString());
+            }
+
+            var result = n % sn == 0 ? "Yes" : "No";
+            Console.WriteLine(result);
+        }
+    }
+}
