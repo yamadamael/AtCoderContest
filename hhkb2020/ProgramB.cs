0e922ea75bdf313bf8c2b4a82db19bc042de8a25
yamadakuniaki <kuniakiymd@gmail.com>
2020-10-11 22:50:47 +0900

    HHKBプロコン ABC解答

---
+++ hhkb2020/ProgramB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(" ");
+            var h = long.Parse(inputs[0]);
+            var w = long.Parse(inputs[1]);
+            var slist = new List<string>();
+
+            var result = 0L;
+            for (var i = 0; i < h; i++)
+            {
+                var s = Console.ReadLine();
+                slist.Add(s);
+                for (var j = 0; j < w - 1; j++)
+                {
+                    if (s[j] == '.' && s[j + 1] == '.')
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            for (var j = 0; j < w; j++)
+            {
+                for (var i = 0; i < h - 1; i++)
+                {
+                    if (slist[i][j] == '.' && slist[i + 1][j] == '.')
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ hhkb2020/ProgramD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC
+{
+    class ProgramD
+    {
+        static void Main(string[] args)
+        {
+            var t = long.Parse(Console.ReadLine());
+            for (var i = 0; i < t; i++)
+            {
+                var inputs = Console.ReadLine().Split(" ");
+                var n = long.Parse(inputs[0]);
+                var a = long.Parse(inputs[1]);
+                var b = long.Parse(inputs[2]);
+                var result = 0L;
+
+
+                Console.WriteLine(result);
+            }
+        }
+
+    }
+}
